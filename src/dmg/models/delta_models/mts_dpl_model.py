608ee40e030d2a28670b6faa6b284d464f3d3446
Leo Lonzarich <lgl5139@psu.edu>
2025-12-08 09:14:10 -0600

    add dpl docstring

--- src/dmg/models/delta_models/mts_dpl_model.py
+++ src/dmg/models/delta_models/mts_dpl_model.py
@@ -1,4 +1,3 @@
-
 from dmg.core.utils.factory import import_phy_model, load_nn_model
 
 import torch
@@ -6,19 +5,50 @@ from typing import Optional, Any
 
 
 class DplModel(torch.nn.Module):
+    """Differentiable parameter learning (dPL) model for MTS.
+
+    Learn parameters for a physics model using a neural network (NN).
+
+    Default modality:
+        Parameterization neural network (NN) -> Physics Model (phy_model)
+
+        - NN: e.g., LSTM, MLP, KNN
+            Learns parameters for the physics model.
+
+        - phy_model: e.g., HBV 1.0, HBV 1.1p.
+            A parameterized physics model that ingests NN-generated parameters
+            and produces some target output. This model must be implemented in a differentiable way to facilitate PyTorch auto-differentiation.
+
+    Parameters
+    ----------
+    phy_model_name
+        The name of the physical model. This allows initialization of multiple
+        physics models from the same config. If not specified, the first
+        model provided in the config is used.
+    phy_model
+        An initialized physics model.
+    nn_model
+        An initialized neural network model.
+    config
+        Configuration settings for the model.
+    device
+        The device to run the model on.
+    """
 
     def __init__(
-            self,
-            phy_model_name: Optional[str] = None,
-            phy_model: Optional[torch.nn.Module] = None,
-            nn_model: Optional[torch.nn.Module] = None,
-            config: Optional[dict[str, Any]] = None,
-            device: Optional[torch.device] = 'cpu',
+        self,
+        phy_model_name: Optional[str] = None,
+        phy_model: Optional[torch.nn.Module] = None,
+        nn_model: Optional[torch.nn.Module] = None,
+        config: Optional[dict[str, Any]] = None,
+        device: Optional[torch.device] = 'cpu',
     ) -> None:
         super().__init__()
         self.name = 'dPL Model'
         self.config = config
-        self.device = device or torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = device or torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu'
+        )
 
         if nn_model and phy_model:
             self.phy_model = phy_model.to(self.device)
@@ -28,8 +58,10 @@ class DplModel(torch.nn.Module):
             self.phy_model = self._init_phy_model(phy_model_name)
             self.nn_model = self._init_nn_model()
         else:
-            raise ValueError("A (1) neural network and physics model or (2)" /
-                             " configuration dictionary is required.")
+            raise ValueError(
+                "A (1) neural network and physics model or (2)"
+                / " configuration dictionary is required."
+            )
 
         self.initialized = True
 
@@ -56,8 +88,10 @@ class DplModel(torch.nn.Module):
         elif self.config['phy_model']:
             model_name = self.config['phy_model']['model'][0]
         else:
-            raise ValueError("A (1) physics model name or (2) model spec in" /
-                             " a configuration dictionary is required.")
+            raise ValueError(
+                "A (1) physics model name or (2) model spec in"
+                / " a configuration dictionary is required."
+            )
 
         model = import_phy_model(model_name)
         return model(self.config['phy_model'], device=self.device)
@@ -93,12 +127,17 @@ class DplModel(torch.nn.Module):
         # Neural network
         ##################### New: Multi-timescale #################
 
-
-
-        if type(self.nn_model).__name__ == 'StackLstmMlpModel' or (hasattr(self.nn_model, '_orig_mod') and type(self.nn_model._orig_mod).__name__ == 'StackLstmMlpModel'):
+        if type(self.nn_model).__name__ == 'StackLstmMlpModel' or (
+            hasattr(self.nn_model, '_orig_mod')
+            and type(self.nn_model._orig_mod).__name__ == 'StackLstmMlpModel'
+        ):
             parameters = self.nn_model(
                 (data_dict['xc_nn_norm_low_freq'], data_dict['c_nn_norm']),
-                (data_dict['xc_nn_norm_high_freq'], data_dict['c_nn_norm'], data_dict['rc_nn_norm'])
+                (
+                    data_dict['xc_nn_norm_high_freq'],
+                    data_dict['c_nn_norm'],
+                    data_dict['rc_nn_norm'],
+                ),
             )
         ############################################################
         elif type(self.nn_model).__name__ == 'LstmMlpModel':
@@ -113,10 +152,3 @@ class DplModel(torch.nn.Module):
         )
 
         return predictions
-
-
-
-
-
-
-
